a758c60a24df36acabc08a201efe3ecc229fd363
Miguel Salvador <miguel_f_r_salvador@hotmail.com>
2023-11-04 21:23:53 +0000

    Section two_two

--- app/src/main/java/miguelsalvador/co/uk/MainActivity.kt
+++ app/src/main/java/miguelsalvador/co/uk/MainActivity.kt
@@ -2,6 +2,10 @@ package miguelsalvador.co.uk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //      Connect to main layout
         setContentView(R.layout.activity_main)
+
+        var btn1 = findViewById<Button>(R.id.btnShowToast);
+        btn1.setOnClickListener{
+//            Log.i("MainActivity","btn was clicked!")
+//           shows alert on app screen
+            Toast.makeText(this,"Button was clicked!",Toast.LENGTH_SHORT).show()
+        }
+    //        btnSendMsgNoActivity.setOnClickListener
     }
 
 
