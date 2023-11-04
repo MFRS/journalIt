25ae6b237dbc9ffb2a27c245cc27aba73dfa630c
Miguel Salvador <miguel_f_r_salvador@hotmail.com>
2023-11-04 19:44:12 +0000

    fg

---
+++ app/src/main/java/miguelsalvador/co/uk/MainActivity.kt
@@ -0,0 +1,15 @@
+package miguelsalvador.co.uk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//      Connect to main layout
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
